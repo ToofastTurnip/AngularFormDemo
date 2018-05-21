59867ee189400a056689916c9cfe4e31c17a8457
Theresa Collins <theresacollins@Theresas-MacBook-Pro.local>
2018-05-21 15:33:22 -0400

    Wizard parts generated

---
+++ src/app/wizard-form/wizard-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wizard-form',
+  templateUrl: './wizard-form.component.html',
+  styleUrls: ['./wizard-form.component.css']
+})
+export class WizardFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
