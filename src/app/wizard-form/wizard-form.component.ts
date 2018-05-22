a96a111fd6d98961a48cef32c68cad50bcca78f3
Theresa Collins <theresacollins@Theresas-MacBook-Pro.local>
2018-05-22 15:46:18 -0400

    Wizard form

--- src/app/wizard-form/wizard-form.component.ts
+++ src/app/wizard-form/wizard-form.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Dingus } from '../dingus';
 
 @Component({
   selector: 'app-wizard-form',
   templateUrl: './wizard-form.component.html',
   styleUrls: ['./wizard-form.component.css']
 })
-export class WizardFormComponent implements OnInit {
+export class WizardFormComponent {
 
-  constructor() { }
+  powers = ['Really Smart', 'Super Flexible',
+            'Super Cool', 'Weather Changer'];
 
-  ngOnInit() {
-  }
+  model = new Dingus(11, 'Toofast', this.powers[0], 'Mitch');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
 
 }
